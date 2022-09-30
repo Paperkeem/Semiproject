44fde475504533be1cefc3268599dd1b09f72b6b
hyun9229 <114386418+hyun9229@users.noreply.github.com>
2022-09-30 15:53:52 +0900

    풀
    풀

---
+++ JspSemiproject/src/main/java/data/Dto/memberDto.java
@@ -0,0 +1,70 @@
+package data.Dto;
+
+import java.sql.Timestamp;
+
+public class memberDto {
+	
+	private String num;
+	private String name;
+	private String id;
+	private String pass;
+	private String type;
+	private String gomin;
+	private String hp;
+	private Timestamp gaipday;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getGomin() {
+		return gomin;
+	}
+	public void setGomin(String gomin) {
+		this.gomin = gomin;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public Timestamp getGaipday() {
+		return gaipday;
+	}
+	public void setGaipday(Timestamp gaipday) {
+		this.gaipday = gaipday;
+	}
+	
+	
+	
+	
+	
+
+}

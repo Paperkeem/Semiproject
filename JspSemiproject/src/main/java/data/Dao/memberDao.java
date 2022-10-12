0e11e2972268eb4be075e281040f59e5f13b54dd
김종이 <ahharlawhddl@gmail.com>
2022-10-12 14:05:00 +0900

    reply / order

--- JspSemiproject/src/main/java/data/Dao/memberDao.java
+++ JspSemiproject/src/main/java/data/Dao/memberDao.java
@@ -86,9 +86,9 @@ DbConnect db=new DbConnect();
 			pstmt.setString(1, dto.getName());
 			pstmt.setString(2, dto.getId());
 			pstmt.setString(3, dto.getPass());
-			pstmt.setString(4, dto.getHp());
-			pstmt.setString(5, dto.getType());
-			pstmt.setString(6, dto.getGomin());
+			pstmt.setString(4, dto.getType());
+			pstmt.setString(5, dto.getGomin());
+			pstmt.setString(6, dto.getHp());
 			
 			
 			pstmt.execute();
@@ -191,5 +191,68 @@ DbConnect db=new DbConnect();
 	
 		return b;
 	}
+	
+	// num 값에 따른 dto 반환
+	public memberDto getOneData(String num) {
+		memberDto dto = new memberDto();
+		
+		Connection conn = db.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql="select * from member where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dto.setNum(rs.getString("num"));
+				dto.setName(rs.getString("name"));
+				dto.setId(rs.getString("id"));
+				dto.setPass(rs.getString("pass"));
+				dto.setHp(rs.getString("hp"));
+				dto.setType(rs.getString("type"));
+				dto.setGomin(rs.getString("gomin"));
+				dto.setGaipday(rs.getTimestamp("gaipday"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		return dto;
+	}
+	
+	//아이디에 대한 넘버값 반환
+		public String getNum(String id)
+		{
+			String num="";
+			
+			Connection conn=db.getConnection();
+			PreparedStatement pstmt=null;
+			ResultSet rs=null;
+			
+			String sql="select * from member where id=?";
+			
+			try {
+				pstmt=conn.prepareStatement(sql);
+				pstmt.setString(1, id);
+				rs=pstmt.executeQuery();
+				
+				if(rs.next())
+					num=rs.getString("num");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				db.dbClose(rs, pstmt, conn);
+			}
+			
+			return num;
+		}
 }
 
